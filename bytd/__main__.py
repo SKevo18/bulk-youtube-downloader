4fa92340a55584bdbb4fa0a83dd449a814fc0809
Kevo <me@kevo.link>
2022-07-11 20:08:06 +0200

    Async

--- bytd/__main__.py
+++ bytd/__main__.py
@@ -1,12 +1,11 @@
 import typing as t
 
-import sys
-import argparse
-import requests
-import os.path
+import typer
+import aiohttp
+import aiofiles
 
-from zipfile import ZipFile
-from shutil import copyfileobj
+from asyncio import Semaphore, sleep, run, wait_for, gather, set_event_loop_policy, WindowsSelectorEventLoopPolicy
+from functools import wraps
 from pathlib import Path
 
 from piped_api import PipedClient
@@ -14,20 +13,28 @@ from piped_api.client import APIError
 
 
 CLIENT = PipedClient()
+"""A Piped client."""
 
 ROOT_PATH = Path(__file__).parent
+"""Root path of the project."""
 TO_DOWNLOAD_PATH = ROOT_PATH / 'to_download.txt'
+"""A list of YouTube IDs to download."""
 DOWNLOAD_INTO = ROOT_PATH / 'downloaded'
+"""To what folder should the files be downloaded?"""
+DOWNLOAD_MB = 5
+"""How much MB to process per download chunk? Choose your average download speed: lower or higher values will make download slower."""
+MAX_TASKS = 5
+"""Maximum number of concurrent downloads."""
 
 TO_DOWNLOAD = set(video_id.strip() for video_id in open(TO_DOWNLOAD_PATH, 'r'))
 
 
 
-def get_stream(video_id: str, type: t.Literal['audio', 'video']='audio') -> t.Optional[t.Tuple[str, str]]:
+def get_stream(video_id: str, stream_type: t.Literal['audio', 'video']='audio') -> t.Optional[t.Tuple[str, str]]:
     """
         Get the best stream from YouTube video ID.
 
-        Returns a tuple of `(video.title, stream_url)`.
+        Returns a tuple of `(<video title>, stream_url)`.
 
         ### Parameters:
         - `video_id` - The ID of the YouTube video.
@@ -41,89 +48,70 @@ def get_stream(video_id: str, type: t.Literal['audio', 'video']='audio') -> t.Op
         print(f'Video "{video_id}" couldn\'t be obtained: {str(api_error).splitlines()[0]}')
         return None
 
-    just_mp4 = [stream for stream in video.get_streams(type=type) if stream.mime_type == f'{type}/mp4' and stream.video_only == False]
+    to_sort = [stream for stream in video.get_streams(stream_type) if stream.mime_type == f'{stream_type}/mp4' and stream.video_only == False]
 
-    return f"{video.uploader} - {video.title}", sorted(just_mp4, key=lambda stream: stream.quality)[0].url
-
-
-
-def download_file(url: str, path: Path) -> None:
-    """
-        Downloads a file from `url` to `path` via [requests](https://2.python-requests.org/en/master/).
-
-        Based on https://stackoverflow.com/a/67053532/ (thanks!).
-        `urlretrive` and literally any other iterator and non-iterator based methods won't download the entire file, so I had to use an alternative approach.
-    """
-
-    total_content_size = int(requests.get(url, stream=True).headers['Content-Length'])
-
-    if os.path.exists(path):
-        temp_size = os.path.getsize(path)
-        if total_content_size == temp_size:
-            return
-
-    else:
-        temp_size = 0
-
-
-    headers = {'Range': f'bytes={temp_size}-'}
-    with requests.get(url, stream=True, headers=headers) as response:
-        response.raise_for_status()
-
-        with open(path, 'ab') as f:
-            copyfileobj(response.raw, f, length=16*1024*1024)
+    return f"{video.uploader} - {video.title}", sorted(to_sort, key=lambda stream: stream.quality)[0].url
 
 
 
-def download_all(to_download: t.List[str]=TO_DOWNLOAD, type: str='audio') -> None:
-    """
-        Downloads all videos in `to_download`.
+async def download(url: str, session: aiohttp.ClientSession, semaphore: Semaphore, download_to: Path):
+    async with semaphore:
+        async with session.get(url, timeout=None) as response:
+            async with aiofiles.open(download_to, 'wb') as f:
+                typer.echo(f"Downloading {download_to.parts[-1]}...")
 
-        ### Parameters:
-        - `to_download` - A list of YouTube video IDs.
-    """
-
-    for video_id in to_download:
-        packed_stream = get_stream(video_id, type=type)
-        if packed_stream is None:
-            continue
-
-        title, url = packed_stream
-        print(f'Downloading "{title}" from `{url}`...')
-
-        download_file(url, DOWNLOAD_INTO / f'{title}.{"mp4" if type == "video" else "mp3"}')
+                try:
+                    async for chunk in response.content.iter_any():
+                        await f.write(chunk)
+                
+                except aiohttp.ClientPayloadError as exception:
+                    typer.echo(f"Couldn't download {download_to.parts[-1]}: {exception}")
+                    download_to.unlink() # remove broken file
 
 
 
-def zip_all(*args, **kwargs):
-    for file in DOWNLOAD_INTO.iterdir():
-        if file.name.endswith(".mp3"):
-            with ZipFile(DOWNLOAD_INTO / 'zipped' / 'audio.zip', 'w', *args, **kwargs) as zip_file:
-                zip_file.write(file)
+def run_async(func: t.Callable):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        async def coro_wrapper():
+            return await func(*args, **kwargs)
 
-        elif file.name.endswith(".mp4"):
-            with ZipFile(DOWNLOAD_INTO / 'zipped' / 'videos.zip', 'w', *args, **kwargs) as zip_file:
-                zip_file.write(file)
+        return run(coro_wrapper)
+
+    return wrapper
 
 
 
-def main():
-    parser = argparse.ArgumentParser(description='Downloads videos from YouTube.')
-    parser.add_argument('--stream-type', '--type', '-t', choices=['audio', 'video'], default='audio', help='The type of stream to download.')
-    parser.add_argument('--zip' ,'-z', action='store_true', help='Zip the downloaded files.')
+def main(stream_type: t.Literal['audio', 'video']='audio'):
+    async def _main():
+        tasks = []
+        semaphore = Semaphore(MAX_TASKS)
 
-    args = parser.parse_args()
+        async with aiohttp.ClientSession() as session:
+            for video_id in TO_DOWNLOAD:
+                packed_stream = get_stream(video_id, stream_type=stream_type)
 
-    download_all(type=args.stream_type)
-    print(f"Downloaded all {'audios' if args.stream_type == 'audio' else 'videos'}...")
+                if packed_stream is None:
+                    print(f"Couldn't obtain any stream for video ID `{video_id}`.")
+                    return
 
-    if args.zip:
-        print("Zipping...")
-        zip_all()
-    
-    print("Done!")
+
+                title, url = packed_stream
+                download_to = DOWNLOAD_INTO / f'{title}.{"mp4" if stream_type == "video" else "mp3"}'
+                if download_to.exists():
+                    typer.echo(f"File {download_to.parts[-1]} already exists, skipping...")
+                    continue
+
+                tasks.append(wait_for(download(url, session, semaphore, download_to), timeout=None))
+
+            await gather(*tasks)
+        
+        typer.echo(f"Downloaded all {stream_type}s...")
+
+    set_event_loop_policy(WindowsSelectorEventLoopPolicy())
+    run(_main())
 
 
 
 if __name__ == "__main__":
-    main()
+    typer.run(main())
